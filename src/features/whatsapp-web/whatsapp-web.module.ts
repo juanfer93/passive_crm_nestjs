b1474a298e8d5b91da401a0020da1d2a1a5412b4
juanfer93 <jfpi_0793@icloud.com>
2026-06-15 15:20:03 -0500

    feat: prepare whatsapp module for shared providers

--- src/features/whatsapp-web/whatsapp-web.module.ts
+++ src/features/whatsapp-web/whatsapp-web.module.ts
@@ -1,8 +1,19 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { WhatsappWebClientService } from './application/services/whatsapp-web-client.service';
 import { WhatsappWebController } from './presentation/controllers/whatsapp-web.controller';
+import { Conversation, ConversationSchema } from '@/features/meta-messaging-webhook/infrastructure/mongo/schemas/conversation.schema';
+import { MessageReceipt, MessageReceiptSchema } from '@/features/meta-messaging-webhook/infrastructure/mongo/schemas/message-receipt.schema';
 
 @Module({
+  imports: [
+    HttpModule.register({ timeout: 8000, maxRedirects: 3 }),
+    MongooseModule.forFeature([
+      { name: Conversation.name, schema: ConversationSchema },
+      { name: MessageReceipt.name, schema: MessageReceiptSchema },
+    ]),
+  ],
   controllers: [WhatsappWebController],
   providers: [WhatsappWebClientService],
   exports: [WhatsappWebClientService],
